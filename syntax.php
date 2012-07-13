795869b4af71a63d64a2277ac145a578cd38b668
Elan Ruusamäe <glen@delfi.ee>
2012-07-13 23:22:41 +0300

    tabs

--- syntax.php
+++ syntax.php
@@ -26,103 +26,103 @@ $conf['plugin']['poldek']['debug'] = true;
  * need to inherit from this class
  */
 class syntax_plugin_poldek extends DokuWiki_Syntax_Plugin {
-    /**
-     * What kind of syntax are we?
-     */
-    function getType() {
-        return 'substition';
-    }
+	/**
+	 * What kind of syntax are we?
+	 */
+	function getType() {
+		return 'substition';
+	}
 
-    /**
-     * Where to sort in?
-     */
-    function getSort() {
-        return 306;
-    }
+	/**
+	 * Where to sort in?
+	 */
+	function getSort() {
+		return 306;
+	}
 
-    /**
-     * Connect pattern to lexer
-     */
-    function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('\{\{poldek>.+?\}\}', $mode, 'plugin_poldek');
-    }
+	/**
+	 * Connect pattern to lexer
+	 */
+	function connectTo($mode) {
+		$this->Lexer->addSpecialPattern('\{\{poldek>.+?\}\}', $mode, 'plugin_poldek');
+	}
 
 
-    /**
-     * Handle the match
-     */
-    function handle($match, $state, $pos, &$handler) {
+	/**
+	 * Handle the match
+	 */
+	function handle($match, $state, $pos, &$handler) {
 		$raw = substr($match, 9, -2);
 
-        $data = array('pkg' => $raw);
-        return $data;
-    }
+		$data = array('pkg' => $raw);
+		return $data;
+	}
 
-    /**
-     * Create output
-     */
-    function render($format, &$renderer, $data) {
-        global $ID;
-        if ($format != 'xhtml') {
-            return false;
-        }
+	/**
+	 * Create output
+	 */
+	function render($format, &$renderer, $data) {
+		global $ID;
+		if ($format != 'xhtml') {
+			return false;
+		}
 
-        global $conf;
-        // get plugin config
-        $c = empty($conf['plugin']['poldek']) ? array() : $conf['plugin']['poldek'];
+		global $conf;
+		// get plugin config
+		$c = empty($conf['plugin']['poldek']) ? array() : $conf['plugin']['poldek'];
 
-        $cachedir = !empty($c['cachedir']) ? $c['cachedir'] : '/tmp/dw-poldek';
+		$cachedir = !empty($c['cachedir']) ? $c['cachedir'] : '/tmp/dw-poldek';
 
-        // base poldek command
-        $poldek = 'poldek -q --skip-installed -O cachedir='.escapeshellarg($cachedir);
+		// base poldek command
+		$poldek = 'poldek -q --skip-installed -O cachedir='.escapeshellarg($cachedir);
 
-        if ($c['repos']) {
-            foreach (explode(',', $c['repos']) as $repo) {
-                $poldek .= ' --sn '.escapeshellarg(trim($repo));
-            }
-        }
+		if ($c['repos']) {
+			foreach (explode(',', $c['repos']) as $repo) {
+				$poldek .= ' --sn '.escapeshellarg(trim($repo));
+			}
+		}
 
-        static $sync = false;
-        if (!$sync) {
-            // sync indexes once per page
-            $cmd = "$poldek --up";
-            // proxies setup
-            if (!empty($c['http_proxy'])) {
-                $cmd .= ' -O '.escapeshellarg("proxy=http: {$c['http_proxy']}");
-            }
-            if (!empty($c['ftp_proxy'])) {
-                $cmd .= ' -O '.escapeshellarg("proxy=ftp: {$c['ftp_proxy']}");
-            }
+		static $sync = false;
+		if (!$sync) {
+			// sync indexes once per page
+			$cmd = "$poldek --up";
+			// proxies setup
+			if (!empty($c['http_proxy'])) {
+				$cmd .= ' -O '.escapeshellarg("proxy=http: {$c['http_proxy']}");
+			}
+			if (!empty($c['ftp_proxy'])) {
+				$cmd .= ' -O '.escapeshellarg("proxy=ftp: {$c['ftp_proxy']}");
+			}
 
-            if ($c['debug']) {
-                error_log($cmd);
-            }
-            exec($cmd, $lines, $rc);
-            $sync = true;
-        }
+			if ($c['debug']) {
+				error_log($cmd);
+			}
+			exec($cmd, $lines, $rc);
+			$sync = true;
+		}
 
-        $cmd = $poldek.' -Q --shcmd='.escapeshellarg("ls {$data['pkg']}");
-        if ($c['debug']) {
-            error_log($cmd);
-        }
-        exec($cmd, $lines, $rc);
-        if ($c['debug']) {
-            error_log("got[".join('\n', $lines)."] -> $rc");
-        }
+		$cmd = $poldek.' -Q --shcmd='.escapeshellarg("ls {$data['pkg']}");
+		if ($c['debug']) {
+			error_log($cmd);
+		}
+		exec($cmd, $lines, $rc);
+		if ($c['debug']) {
+			error_log("got[".join('\n', $lines)."] -> $rc");
+		}
 
-        if ($rc) {
-            if (preg_match('/^error:.*no such package or directory/', $lines[0])) {
-                $renderer->doc .= join(' ', $lines);
-            } else {
-                dbglog($lines);
-                $renderer->doc .= "<b style='color: red'>poldek error, see debug.log for details</b>";
-            }
-        } else {
-            $renderer->doc .= join("\n", $lines);
-        }
+		if ($rc) {
+			if (preg_match('/^error:.*no such package or directory/', $lines[0])) {
+				$renderer->doc .= join(' ', $lines);
+			} else {
+				dbglog($lines);
+				$renderer->doc .= "<b style='color: red'>poldek error, see debug.log for details</b>";
+			}
+		} else {
+			$renderer->doc .= join("\n", $lines);
+		}
 
-        return true;
-    }
+		return true;
+	}
 }
 
-//Setup VIM: ex: et ts=4 enc=utf-8 :
+//Setup VIM: ex: noet ts=4 enc=utf-8 :
