445ac2dfcc1f26c0d5b13a65fd26be7380e8fbd0
Elan Ruusamäe <glen@delfi.ee>
2012-07-15 15:35:56 +0300

    add language file for configuration manager

---
+++ lang/en/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * English language file
+ *
+ * @author     Elan Ruusamäe <glen@delfi.ee>
+ */
+ 
+// for the configuration manager
+$lang['cachedir'] = 'Cache dir for poldek indexes';
+$lang['http_proxy'] = 'Optional HTTP proxy for poldek';
+$lang['ftp_proxy'] = 'Optional FTP proxy for poldek';
+$lang['repos'] = 'Poldek repositories to query, separated by comma';
